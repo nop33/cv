58d1a88890439554e0fcb6d4e90378c3dc40e39e
Ilias Trichopoulos <hlias.nop@gmail.com>
2021-05-25 22:24:50 +0200

    Upgrade all packages

--- build.js
+++ build.js
@@ -50,7 +50,7 @@ function buildContent() {
 
         if (fs.existsSync(contentFilePath)) {
           let key = `${component}_component_content`;
-          let value = yaml.safeLoad(fs.readFileSync(contentFilePath, 'utf8'));
+          let value = yaml.load(fs.readFileSync(contentFilePath, 'utf8'));
 
           content[key] = value;
         }
